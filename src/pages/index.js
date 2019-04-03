e4765bb430246be8d85f4ef9f682f2d05954110c
Alex Bennett <alexandergraybennett@gmail.com>
2019-04-03 09:07:25 -0600

    doing styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Link, graphql } from 'gatsby'
 import Layout from '../components/Layout'
+import '../css/headers.css'
 
 export default class IndexPage extends React.Component {
   render() {
