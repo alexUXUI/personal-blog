c26d8322e7f9788aaed3b099d5c4db82ede95b30
Alex Bennett <alexandergraybennett@gmail.com>
2019-06-25 17:57:35 -0600

    rm optional configs

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,10 +14,6 @@ module.exports = {
         // Include GTM in development.
         // Defaults to false meaning GTM will only be loaded in production.
         includeInDevelopment: false,
-
-        // Specify optional GTM environment details.
-        gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIROMENT_AUTH_STRING",
-        gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIROMENT_PREVIEW_NAME",
       },
     },
     {
